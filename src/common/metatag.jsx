00a8059685c1d50991866da02a521426dcfab22d
gaurav23617 <gaurav23617@gmail.com>
2024-06-08 22:27:33 +0530

    "Updated MetaTag component to include additional metadata and modified metadata.json to simplify page metadata structure"

--- src/common/metatag.jsx
+++ src/common/metatag.jsx
@@ -9,21 +9,33 @@ const MetaTag = ({ title, description, imageUrl, imageAlt }) => (
     <meta property="og:title" content={title} />
     <meta name="description" content={description} />
     <meta property="og:description" content={description} />
-    <meta property="og:image" content={metaData.name.hostname + imageUrl} />
-    <meta property="og:url"content={metaData.hostname + window.location.pathname + window.location.search} />
-    <meta name="twitter:card" content="twiter" />
-    <meta name="twitter:image:alt" content={imageAlt} />
+    <meta property="og:image" content={metaData.hostname + imageUrl} />
+    <meta name="keywords" content={keywords} />
+    <meta property="og:url"content={metaData.name.hostname + window.location.pathname + window.location.search} />
+    
+    
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    <meta name="linkedin:image" content={imageUrl} />
+    <meta name="twitter:image:alt" content={imageUrl} />
     <meta name="twitter:site" content={metaData.name.twitterUsername} />
+
+
     <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+    <meta name="author" content={metaData.name.hostname} />
+    <link rel="canonical" href={metaData.name.hostname} />
+    <meta property="og:site_name" content={metaData.name.sitename} />
     <meta name="robots" content="noindex, follow" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="icon" href="/favicon.png" />
+    <link rel="icon" type="image/x-icon" href="/favicon.png" />
   </Head>
 );
 
 SEO.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  keywords: PropTypes.string,
   imageUrl: PropTypes.string,
   imageAlt: PropTypes.string,
 };
