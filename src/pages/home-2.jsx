455cfa2a4f83d6139cb7f8989d52d2b2bb297ffa
gaurav23617 <gaurav23617@gmail.com>
2024-06-12 14:06:56 +0530

    "Swap Home and HomeTwo components in home-2.jsx and index.jsx files"

--- src/pages/home-2.jsx
+++ src/pages/home-2.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import MetaTag from '../common/metatag';
 import HomeTwo from '../components/homes/home-2';
 import WrapperTwo from '../layout/wrapper-2';
+import Home from '../components/homes/home';
 
 const index = () => {
     return (
         <WrapperTwo>
             <MetaTag pageTitle={"Home-2"} />
-            <HomeTwo />
+            <Home/>
         </WrapperTwo>
     );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,15 @@ import MetaTag from "../common/metatag";
 import { metaData } from "../data/metadata";
 import Home from "../components/homes/home";
 import Wrapper from "../layout/wrapper";
+import WrapperTwo from '../layout/wrapper-2';
+import HomeTwo from "../components/homes/home-2";
 
 
 const index = () => {
   return (
     <Wrapper>
       <MetaTag title={metaData.home.title} />
-      <Home />
+      <HomeTwo />
     </Wrapper>
   );
 };
