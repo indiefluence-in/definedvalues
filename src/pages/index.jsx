2b664e126aee3b4cb785128841db22779f0c6224
gaurav23617 <gaurav23617@gmail.com>
2024-06-18 10:37:24 +0530

    "Updated API response in hello.js and removed imports in index.jsx"

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,6 @@
 import MetaTag from "../common/metatag";
 import { metaData } from "../data/metadata";
-import Home from "../components/homes/home";
 import Wrapper from "../layout/wrapper";
-import WrapperTwo from '../layout/wrapper-2';
 import HomeTwo from "../components/homes/home-2";
 
 
